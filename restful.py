990bcf64418bf414e270d596c000446ed271b27d
Tony Plutony <cutefomlower1g0@hotmail.com>
2025-08-13 14:26:38 +0200

    Create restful.py

---
+++ restful.py
@@ -0,0 +1,31 @@
+### **Repository 2: RESTful API with Flask**
+```python
+# Repository: python-flask-api
+# Description: A RESTful API for managing users.
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+users = []
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    """Get all users."""
+    return jsonify(users)
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    """Add a new user."""
+    data = request.get_json()
+    users.append(data)
+    return jsonify({"message": "User added", "user": data}), 201
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    """Delete a user by ID."""
+    global users
+    users = [user for user in users if user.get('id') != user_id]
+    return jsonify({"message": "User deleted"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
