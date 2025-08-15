ed4504573618751fba4f5eca651c3c951931293b
Tony Plutony <cutefomlower1g0@hotmail.com>
2025-08-15 14:37:20 +0200

    Update restful.py
    Enhancement: Added PUT endpoint for updating user details

--- restful.py
+++ restful.py
@@ -13,12 +13,14 @@ def get_users():
     """Get all users."""
     return jsonify(users)
 
-@app.route('/users', methods=['POST'])
-def add_user():
-    """Add a new user."""
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
     data = request.get_json()
-    users.append(data)
-    return jsonify({"message": "User added", "user": data}), 201
+    for user in users:
+        if user.get('id') == user_id:
+            user.update(data)
+            return jsonify({"message": "User updated"})
+    return jsonify({"error": "User not found"}), 404
 
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
